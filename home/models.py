596e2e9ff5f7fc7a94ef90fa4effc101a3cfeffd
Crowdbotics <team@crowdbotics.com>
2020-08-25 05:13:47 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,27 +7,107 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150,)
+    tests = models.OneToOneField(
+        "home.CustomText",
+        null=True,
+        blank=True,
+        default=1,
+        on_delete=models.SET_DEFAULT,
+        related_name="customtext_tests",
+    )
+    user = models.OneToOneField(
+        "home.CustomText",
+        null=True,
+        blank=True,
+        default=2,
+        on_delete=models.SET_DEFAULT,
+        related_name="customtext_user",
+    )
+    new = models.OneToOneField(
+        "home.CustomText",
+        null=True,
+        blank=True,
+        default=1,
+        on_delete=models.SET_DEFAULT,
+        related_name="customtext_new",
+    )
+    test = models.ForeignKey(
+        "home.CustomText",
+        null=True,
+        blank=True,
+        default=1,
+        on_delete=models.SET_DEFAULT,
+        related_name="customtext_test",
+    )
+    key2 = models.ForeignKey(
+        "home.CustomText",
+        null=True,
+        blank=True,
+        default=1,
+        on_delete=models.SET_DEFAULT,
+        related_name="customtext_key2",
+    )
+    key1 = models.ForeignKey(
+        "home.CustomText",
+        null=True,
+        blank=True,
+        default=2,
+        on_delete=models.SET_DEFAULT,
+        related_name="customtext_key1",
+    )
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
     body = models.TextField()
+    key1 = models.ForeignKey(
+        "users.User",
+        null=True,
+        blank=True,
+        default=1,
+        on_delete=models.SET_DEFAULT,
+        related_name="homepage_key1",
+    )
+    key2 = models.ForeignKey(
+        "home.CustomText",
+        null=True,
+        blank=True,
+        default=2,
+        on_delete=models.SET_DEFAULT,
+        related_name="homepage_key2",
+    )
+    key3 = models.OneToOneField(
+        "users.User",
+        null=True,
+        blank=True,
+        default=1,
+        on_delete=models.SET_DEFAULT,
+        related_name="homepage_key3",
+    )
+    key4 = models.OneToOneField(
+        "users.User",
+        null=True,
+        blank=True,
+        default=4,
+        on_delete=models.SET_DEFAULT,
+        related_name="homepage_key4",
+    )
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
@@ -5,10 +6,39 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
-
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    name = models.CharField(_("Name of User"), blank=True, null=True, max_length=255)
+    name = models.CharField(null=True, blank=True, max_length=255,)
+    key1 = models.OneToOneField(
+        "home.CustomText",
+        null=True,
+        blank=True,
+        default=1,
+        on_delete=models.SET_DEFAULT,
+        related_name="user_key1",
+    )
+    key2 = models.OneToOneField(
+        "home.CustomText",
+        null=True,
+        blank=True,
+        default=2,
+        on_delete=models.SET_DEFAULT,
+        related_name="user_key2",
+    )
+    key3 = models.ForeignKey(
+        "home.CustomText",
+        null=True,
+        blank=True,
+        default=3,
+        on_delete=models.SET_DEFAULT,
+        related_name="user_key3",
+    )
+    key4 = models.ForeignKey(
+        "home.CustomText",
+        null=True,
+        blank=True,
+        default=4,
+        on_delete=models.SET_DEFAULT,
+        related_name="user_key4",
+    )
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
